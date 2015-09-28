c4650e51be903b1994933b4a3b1d16f0039c9c94
Marc Easen <marc@easen.co.uk>
2015-09-28 19:39:12 +0100

    Restructure the sonos to work on a raspberry pi (slower CPUs)

--- apps/sonos.js
+++ apps/sonos.js
@@ -1,42 +1,43 @@
 var sonos = require('sonos');
 var Q = require('q');
 var _ = require('underscore');
+var debug = require('debug')('pconnect:sonos');
 
 var TIMEOUT = 2000;
 var devices = [];
 
-function getZones (deviceList) {
-  var zones = []
-  deviceList.forEach(function (device) {
-    if (zones.indexOf(device.CurrentZoneName) === -1 && device.CurrentZoneName !== 'BRIDGE' && undefined !== device.CurrentZoneName) {
-      zones.push(device.CurrentZoneName)
-    }
-  })
-  return zones
+function getZones(deviceList) {
+    var zones = []
+    deviceList.forEach(function (device) {
+        if (zones.indexOf(device.CurrentZoneName) === -1 && device.CurrentZoneName !== 'BRIDGE' && undefined !== device.CurrentZoneName) {
+            zones.push(device.CurrentZoneName)
+        }
+    })
+    return zones;
 }
 
-function getZoneDevices (zone, deviceList) {
-  var zoneDevices = []
-  deviceList.forEach(function (device) {
-    if (device.CurrentZoneName === zone) {
-      zoneDevices.push(device)
-    }
-  })
-  return zoneDevices
+function getZoneDevices(zone, deviceList) {
+    var zoneDevices = []
+    deviceList.forEach(function (device) {
+        if (device.CurrentZoneName === zone) {
+            zoneDevices.push(device)
+        }
+    })
+    return zoneDevices;
 }
 
-function getZoneCoordinator (zone, deviceList) {
-  var coordinator
-  deviceList.forEach(function (device) {
-    if (device.CurrentZoneName === zone && device.coordinator === 'true') {
-      coordinator = device
-    }
-  })
-  return coordinator
+function getZoneCoordinator(zone, deviceList) {
+    var coordinator;
+    deviceList.forEach(function (device) {
+        if (device.CurrentZoneName === zone && device.coordinator === 'true') {
+            coordinator = device;
+        }
+    })
+    return coordinator;
 }
 
 
-var search = function() {
+var search = function () {
     var devices = new Array();
     var search = sonos.search();
     search.on('DeviceAvailable', function (device, model) {
@@ -46,7 +47,7 @@ var search = function() {
 };
 
 var devices = search();
-var SonosApp = function(zone, coordinator, devices) {
+var SonosApp = function (zone, coordinator, devices) {
     this.zone = zone;
     this.coordinator = coordinator;
     this.devices = devices;
@@ -93,7 +94,7 @@ SonosApp.prototype = {
                     if (volumneStep === undefined) {
                         volumneStep = 3;
                     }
-                    self.coordinator.getVolume(function(error, vol) {
+                    self.coordinator.getVolume(function (error, vol) {
                         vol += volumneStep;
 
                         if (vol > 100) {
@@ -102,7 +103,7 @@ SonosApp.prototype = {
                             vol = 0;
                         }
 
-                        self.coordinator.setVolume(vol, function() {
+                        self.coordinator.setVolume(vol, function () {
                             resolve(202);
                         });
                     });
@@ -115,37 +116,47 @@ SonosApp.prototype = {
     }
 };
 
+var loadAllDevices = Q.Promise(function (resolve) {
+    var devices = new Array();
+    debug('Looking for Sonos devices...');
+    sonos.search({timeout: TIMEOUT}, function (device, model) {
+        debug('Received Sonos device [%s]', device.host);
 
-sonos.search({timeout: TIMEOUT}, function (device, model) {
-  var data = {ip: device.host, port: device.port, model: model, device: device}
-
-  device.getZoneAttrs(function (err, attrs) {
-    if (!err) {
-      _.extend(data, attrs)
-    }
-    device.getZoneInfo(function (err, info) {
-      if (!err) {
-        _.extend(data, info)
-      }
-      device.getTopology(function (err, info) {
-        if (!err) {
-          info.zones.forEach(function (group) {
-            if (group.location === 'http://' + data.ip + ':' + data.port + '/xml/device_description.xml') {
-              _.extend(data, group)
+        var data = {ip: device.host, port: device.port, model: model, device: device}
+        device.getZoneAttrs(function (err, attrs) {
+            if (!err) {
+                _.extend(data, attrs);
             }
-          })
-        }
-        devices.push(data)
-      })
-    })
-  })
+            device.getZoneInfo(function (err, info) {
+                if (!err) {
+                    _.extend(data, info);
+                }
+                device.getTopology(function (err, info) {
+                    if (!err) {
+                        info.zones.forEach(function (group) {
+                            if (group.location === 'http://' + data.ip + ':' + data.port + '/xml/device_description.xml') {
+                                _.extend(data, group);
+                            }
+                        })
+                    }
+                    devices.push(data);
+                });
+            });
+        });
+    });
+
+    setTimeout(function() {
+        resolve(devices);
+    }, TIMEOUT + 1000);
 });
 
+
+
 module.exports.loadApp = Q.Promise(function (resolve) {
-    var zones = new Array();
-    setTimeout(function () {
+    loadAllDevices.then(function(devices) {
+        var zones = new Array();
         getZones(devices).forEach(function (zone) {
-            var coordinator = getZoneCoordinator(zone, devices)
+            var coordinator = getZoneCoordinator(zone, devices);
             zones.push(
                 new SonosApp(
                     zone,
@@ -155,5 +166,6 @@ module.exports.loadApp = Q.Promise(function (resolve) {
             );
         });
         resolve(zones);
-    }, TIMEOUT);
+        debug('Resolved Sonos zones');
+    });
 });
